0475022fb6b8f0e5b8d97232a00e06ed2901272a
Bulbul89 <bulbul.ei2236@gmail.com>
2023-06-10 15:22:16 +0600

    2

--- src/components/usersList.js
+++ src/components/usersList.js
@@ -1,4 +1,13 @@
-function UsersList () {
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../thunks/fetchUsers";
+
+function UsersList() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    },[dispatch])
     return <div> 
         UsersList
     </div>
