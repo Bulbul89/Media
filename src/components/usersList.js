72a072f221105bc99a11b0cc760b229a8bf06b5a
Bulbul89 <bulbul.ei2236@gmail.com>
2023-06-15 16:47:54 +0600

    6/15/2023 4.47PM

--- src/components/usersList.js
+++ src/components/usersList.js
@@ -23,11 +23,7 @@ function UsersList() {
    if(isLoading){
         return <Skeleton times={6} className='h-10 w-full'/>
     } 
-    if(error){
-       return <div>
-            error fetching data.....
-        </div>
-    }
+   
     const renderedUsers = data.map( (user) => {
         return <div key={user.id} className="mb-2 border rounded">
             <div className="flex p-2 justify-between items-center cursor-pointer">
@@ -35,6 +31,14 @@ function UsersList() {
             </div>
         </div>
     })
+
+    if(error){
+        return <div>
+             error fetching data.....
+         </div>
+    }
+    
+    
     return (<div> 
         <div  className="flex flex-row justify-between m-3">  
             <h1 className="m-2 text-xl">Users</h1>
